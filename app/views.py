0e68ec0dcfd668118df27e82e0a5feaf2a618d85
yasmeen123456789 <yasmeenyassu3@gmail.com>
2023-05-19 21:07:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def first(request):
+    return HttpResponse("this is first view base")
+    
+def second(request):
+    return HttpResponse("this is second view base")
